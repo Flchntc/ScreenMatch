d32c261a0f15aa3c10ac2a2c1e26dfc3d130785c
Flchntc <arturviniciuslisboa@gmail.com>
2024-03-24 17:40:36 -0300

    Implementação de buscas na base de dados através da linguagem JPA e de queries nativas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,9 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.model.Episodio;
-import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.model.*;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
@@ -26,6 +23,8 @@ public class Principal {
 
     private List<Serie> series = new ArrayList<>();
 
+    private Optional<Serie> serieBusca;
+
     public Principal(SerieRepository repositorio) {
         this.repositorio = repositorio;
     }
@@ -37,6 +36,14 @@ public class Principal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar series buscadas
+                    4 - Buscar séries por titulo
+                    5 - Buscar séries por ator
+                    6 - Buscar top 5 séries
+                    7 - Buscar séries por categoria
+                    8 - Buscar séries por número de temporadas
+                    9 - Buscar episódio por trecho do título
+                    10 - Buscar top 5 episódios de determinada série
+                    11 - Buscar episodios por data de lançamento
                                     
                     0 - Sair                                
                     """;
@@ -52,9 +59,32 @@ public class Principal {
                 case 2:
                     buscarEpisodioPorSerie();
                     break;
-
                 case 3:
                     listarSeriesBuscadas();
+                    break;
+                case 4:
+                    buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriesPorAtor();
+                    break;
+                case 6:
+                    buscarTopSeries();
+                    break;
+                case 7:
+                    buscarSeriesPorCategoria();
+                    break;
+                case 8:
+                    buscarSeriesPorTemporada();
+                    break;
+                case 9:
+                    buscarEpisodioPorTrecho();
+                    break;
+                case 10:
+                    topEpisodiosPorSerie();
+                    break;
+                case 11:
+                    buscarEpisodiosPorData();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -64,6 +94,7 @@ public class Principal {
             }
         }
     }
+
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
         Serie serie = new Serie(dados);
@@ -82,7 +113,7 @@ public class Principal {
 
     private void buscarEpisodioPorSerie(){
         listarSeriesBuscadas();
-        System.out.println("Digite o nome da serie para a busca dos episodios");
+        System.out.println("Escolha uma série pelo nome:");
         var nomeSerie = leitura.nextLine();
 
         Optional<Serie> serie = series.stream()
@@ -117,4 +148,92 @@ public class Principal {
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
     }
+
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Digite o nome da serie para a busca dos episodios");
+        var nomeSerie = leitura.nextLine();
+        serieBusca = repositorio.findByTitleContainingIgnoreCase(nomeSerie);
+
+        if(serieBusca.isPresent()){
+            System.out.println("Dados da série: " + serieBusca.get());
+        } else {
+            System.out.println("Dados da série nao encontrados: ");
+        }
+    }
+
+    private void buscarSeriesPorAtor() {
+        System.out.println("Qual nome para busca?");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de que valor ?");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEcontradas = repositorio.findByAtoresContainingIgnoreCaseAndRatingGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que " +nomeAtor + "trabalhou");
+        seriesEcontradas.forEach(s ->
+                System.out.println(s.getTitle() + " avaliação: " + s.getRating()));
+
+    }
+
+    private void buscarTopSeries() {
+        List<Serie> serieTop = repositorio.findTop5ByOrderByRatingDesc();
+        serieTop.forEach(s ->
+                System.out.println(s.getTitle() + " avaliação: " + s.getRating()));
+    }
+
+    private void buscarSeriesPorCategoria() {
+        System.out.println("Deseja buscar séries de que categoria/gênero? ");
+        var nomeGenero = leitura.nextLine();
+        Categoria categoria = Categoria.fromPortugues(nomeGenero);
+        List<Serie> seriesPorCategoria = repositorio.findByGenero(categoria);
+        System.out.println("Series da categoria " + nomeGenero);
+        seriesPorCategoria.forEach(System.out::println);
+    }
+
+    private void buscarSeriesPorTemporada() {
+        System.out.println("Até quantas temporadas para a busca? ");
+        var totalSeasons = leitura.nextInt();
+        leitura.nextLine();
+        System.out.println("Avaliações a partir de que valor ?");
+        var avaliacao = leitura.nextDouble();
+
+        List <Serie> seriesPorTemporada = repositorio.seriesPorTemporadaEAvaliacao(totalSeasons,avaliacao);
+        System.out.println("Séries com até " +totalSeasons + " temporadas encontradas");
+        seriesPorTemporada.forEach(s ->
+                System.out.println(s.getTitle() + ", avaliação: " + s.getRating()));
+    }
+
+    private void buscarEpisodioPorTrecho() {
+        System.out.println("Qual nome do episódio para busca?");
+        var trechoEpisodio = leitura.nextLine();
+        List<Episodio> episodiosEcontrados = repositorio.episodiosPorTrecho(trechoEpisodio);
+        episodiosEcontrados.forEach(e ->
+                System.out.printf("Série: %s Temporada %s - Episódio %s - %s\n",
+                        e.getSerie().getTitle(), e.getTemporada(),
+                        e.getEpNum(), e.getTitle()));
+    }
+
+    private void topEpisodiosPorSerie() {
+        buscarSeriePorTitulo();
+        if(serieBusca.isPresent()){
+            Serie serie = serieBusca.get();
+            List<Episodio> topEpisodios = repositorio.topEpisodiosPorSerie(serie);
+            topEpisodios.forEach(e ->
+                    System.out.printf("Série: %s Temporada %s - Episódio %s - %s - Avaliação %.1f\n",
+                            e.getSerie().getTitle(), e.getTemporada(),
+                            e.getEpNum(), e.getTitle(), e.getRating()));
+        }
+    }
+    private void buscarEpisodiosPorData() {
+        buscarSeriePorTitulo();
+        if(serieBusca.isPresent()){
+            Serie serie = serieBusca.get();
+            System.out.println("Digite o ano limite de lançamento");
+            var anoLancamento = leitura.nextInt();
+            leitura.nextLine();
+
+            List<Episodio> episodiosAno = repositorio.episodiosPorSerieEAno(serie, anoLancamento);
+            episodiosAno.forEach(System.out::println);
+        }
+    }
+
 }
--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -1,7 +1,32 @@
 package br.com.alura.screenmatch.repository;
 
+import br.com.alura.screenmatch.model.Categoria;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional <Serie> findByTitleContainingIgnoreCase(String nomeSerie);
+
+    List<Serie> findByAtoresContainingIgnoreCaseAndRatingGreaterThanEqual(String nomeAtor, Double rating);
+
+    List<Serie> findTop5ByOrderByRatingDesc();
+
+    List<Serie> findByGenero(Categoria categoria);
+
+    @Query("SELECT s FROM Serie s WHERE s.totalSeasons <= :totalSeasons AND s.rating >= :rating")
+    List<Serie> seriesPorTemporadaEAvaliacao(Integer totalSeasons, Double rating);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.title ILIKE %:trechoEpisodio%")
+    List<Episodio> episodiosPorTrecho(String trechoEpisodio);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie ORDER BY e.rating DESC LIMIT 5")
+    List<Episodio> topEpisodiosPorSerie(Serie serie);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie AND YEAR(e.releaseDate) >= :anoLancamento")
+    List<Episodio> episodiosPorSerieEAno(Serie serie, int anoLancamento);
 }
